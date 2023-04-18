f3913e5e39a81f904e757b1e73265f68ffeb5002
Akpro865 <infinityak865@gmail.com>
2023-04-18 18:03:34 +0530

    navbar base design

--- src/comps/Navbar.jsx
+++ src/comps/Navbar.jsx
@@ -1,24 +1,72 @@
 import Search from './Search'
 import { RiHome6Fill } from 'react-icons/ri'
-import { TbTrees } from 'react-icons/tb'
+import { TbTrees, TbClockPlay } from 'react-icons/tb'
 import { BsPeopleFill } from 'react-icons/bs'
+import { SlCalender } from 'react-icons/sl'
+import { MdOutlineMarkUnreadChatAlt, MdOutlineInterests, MdNotificationsActive } from 'react-icons/md'
+import { FiPlayCircle, FiChevronDown } from 'react-icons/fi'
+import profile from '../assets/profile.jpeg'
 
 function Navbar() {
   return (
     <div className='flex'>
-      <section className='bg-green-100 flex items-center flex-1'>
-        <div>
+      <section className='flex items-center flex-1'>
+        <div className='w-1/6'>
           <RiHome6Fill />
         </div>
         <Search />
       </section>
 
-      <div className='flex-1'>
+      <div className='flex-1 flex items-center text-[28px]'>
+       <div className='flex flex-1 items-center'>        
         <TbTrees />
+        <div className='flex flex-col'>
+         <span className='text-[14px]'>Roots</span>
+         <span className='text-[12px]'>Connect Friends</span>
+        </div>
+       </div>
+       <div className='flex flex-1 items-center'>        
+        <TbClockPlay />
+        <div className='flex flex-col'>
+         <span className='text-[14px]'>Kicks</span>
+         <span className='text-[12px]'>Short Videos</span>
+        </div>
+       </div>
+       <div className='flex flex-1 items-center'>        
+        <SlCalender />
+        <div className='flex flex-col'>
+         <span className='text-[14px]'>U-Meet</span>
+         <span className='text-[12px]'>Create Events</span>
+        </div>
+       </div>        
       </div>
 
       <div className='flex-1'>
-        <BsPeopleFill />
+       <div className='flex items-center justify-evenly text-[26px]'>
+        <span className='flex flex-col items-center'>
+         <BsPeopleFill className=''/>
+         <span className='text-[12px]'>Friends</span>
+        </span>
+        <span className='flex flex-col items-center'>
+         <MdOutlineMarkUnreadChatAlt />
+         <span className='text-[12px]'>Chat</span>
+        </span>
+        <span className='flex flex-col items-center'>
+         <FiPlayCircle />
+         <span className='text-[12px]'>U-Stream</span>
+        </span>
+        <span className='flex flex-col items-center'>
+         <MdOutlineInterests />
+         <span className='text-[12px]'>Interests</span>
+        </span>
+        <span className='flex flex-col items-center'>
+         <MdNotificationsActive />
+         <span className='text-[12px]'>Notifications</span>
+        </span>
+        <span>
+          <img src={profile} className='w-9 h-9 rounded-full object-cover' />
+        </span>
+       </div>
       </div>
     </div>
   )
--- src/comps/Search.jsx
+++ src/comps/Search.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import { AiOutlineSearch } from 'react-icons/ai'
 
 function Search() {
   return (
-    <div>Search</div>
+    <div className='w-5/6 flex justify-center'>
+     <div className='w-4/6 bg-gray-200 flex items-center justify-center rounded-[9px]'>
+       <input type='search' placeholder='Search...' className='w-full bg-gray-200 rounded-[9px] outline-none h-8 px-2' />
+       <AiOutlineSearch className='mx-1.5 text-[17px]'/>
+     </div>
+    </div>
   )
 }
 
