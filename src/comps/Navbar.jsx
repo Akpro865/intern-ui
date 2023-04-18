a27ed5e922b66527467f7fcd7d4cca3abc53bdcf
Akpro865 <infinityak865@gmail.com>
2023-04-18 19:03:08 +0530

    navbar design done

--- src/comps/Navbar.jsx
+++ src/comps/Navbar.jsx
@@ -9,39 +9,39 @@ import profile from '../assets/profile.jpeg'
 
 function Navbar() {
   return (
-    <div className='flex'>
-      <section className='flex items-center flex-1'>
-        <div className='w-1/6'>
+    <div className='flex pt-2'>
+      <section className='flex items-center flex-1 border-b-[6px]'>
+        <div className='w-1/6 pl-6 flex justify-center text-4xl text-blue-800'>
           <RiHome6Fill />
         </div>
         <Search />
       </section>
 
-      <div className='flex-1 flex items-center text-[28px]'>
-       <div className='flex flex-1 items-center'>        
-        <TbTrees />
+      <div className='flex-1 flex bg-[#e4e7ec] rounded-t-md pb-[6px]'>
+       <div className='flex flex-1 items-center bg-[#cdcdcd] mx-1 ml-2 mt-2 rounded-t-md'>        
+        <TbTrees className='mx-2 h-8 w-8 text-gray-700'/>
         <div className='flex flex-col'>
-         <span className='text-[14px]'>Roots</span>
-         <span className='text-[12px]'>Connect Friends</span>
+         <span className='text-[14px] text-gray-700 font-semibold'>Roots</span>
+         <span className='text-[11px]'>Connect Friends</span>
         </div>
        </div>
-       <div className='flex flex-1 items-center'>        
-        <TbClockPlay />
+       <div className='flex flex-1 items-center bg-[#cdcdcd] mx-1 mt-2 rounded-t-md'>        
+        <TbClockPlay className='mx-2 h-8 w-8 text-gray-700'/>
         <div className='flex flex-col'>
-         <span className='text-[14px]'>Kicks</span>
-         <span className='text-[12px]'>Short Videos</span>
+         <span className='text-[14px] text-gray-700 font-semibold'>Kicks</span>
+         <span className='text-[11px]'>Short Videos</span>
         </div>
        </div>
-       <div className='flex flex-1 items-center'>        
-        <SlCalender />
+       <div className='flex flex-1 items-center bg-[#cdcdcd] mx-1 mt-2 mr-2 rounded-t-md'>        
+        <SlCalender className='mx-2 h-8 w-8 text-gray-700'/>
         <div className='flex flex-col'>
-         <span className='text-[14px]'>U-Meet</span>
-         <span className='text-[12px]'>Create Events</span>
+         <span className='text-[14px] text-gray-700 font-semibold'>U-Meet</span>
+         <span className='text-[11px]'>Create Events</span>
         </div>
        </div>        
       </div>
 
-      <div className='flex-1'>
+      <div className='flex-1 text-[#707070] border-b-[6px]'>
        <div className='flex items-center justify-evenly text-[26px]'>
         <span className='flex flex-col items-center'>
          <BsPeopleFill className=''/>
@@ -49,22 +49,23 @@ function Navbar() {
         </span>
         <span className='flex flex-col items-center'>
          <MdOutlineMarkUnreadChatAlt />
-         <span className='text-[12px]'>Chat</span>
+         <span className='text-[11px]'>Chat</span>
         </span>
         <span className='flex flex-col items-center'>
          <FiPlayCircle />
-         <span className='text-[12px]'>U-Stream</span>
+         <span className='text-[11px]'>U-Stream</span>
         </span>
         <span className='flex flex-col items-center'>
          <MdOutlineInterests />
-         <span className='text-[12px]'>Interests</span>
+         <span className='text-[11px]'>Interests</span>
         </span>
         <span className='flex flex-col items-center'>
          <MdNotificationsActive />
-         <span className='text-[12px]'>Notifications</span>
+         <span className='text-[11px]'>Notifications</span>
         </span>
-        <span>
+        <span className='flex flex-col items-center'>
           <img src={profile} className='w-9 h-9 rounded-full object-cover' />
+          <FiChevronDown className='text-[14px]'/>
         </span>
        </div>
       </div>
--- src/comps/Search.jsx
+++ src/comps/Search.jsx
@@ -2,9 +2,9 @@ import { AiOutlineSearch } from 'react-icons/ai'
 
 function Search() {
   return (
-    <div className='w-5/6 flex justify-center'>
-     <div className='w-4/6 bg-gray-200 flex items-center justify-center rounded-[9px]'>
-       <input type='search' placeholder='Search...' className='w-full bg-gray-200 rounded-[9px] outline-none h-8 px-2' />
+    <div className='w-full flex justify-center'>
+     <div className='w-5/6 bg-gray-200 flex items-center justify-center rounded-[9px]'>
+       <input type='search' placeholder='Search...' className='w-full bg-gray-200 rounded-[9px] outline-none h-9 px-2' />
        <AiOutlineSearch className='mx-1.5 text-[17px]'/>
      </div>
     </div>
